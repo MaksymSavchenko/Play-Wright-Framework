d3378c94bc28315b0fa10f64402d57f15802220f
Maksym Savchenko <maxsavchenko@gmail.com>
2024-04-03 15:53:37 -0700

    Refactored files naming

--- tests/codeGenAuto.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('https://www.kiwico.com/');
-  await page.locator('#subnav-by-age').getByRole('link', { name: '-3' }).click();
-  await page.getByRole('button', { name: 'Close' }).click();
-  await page.locator('div:nth-child(8) > div > div > .jsx-1385608643').click();
-});
